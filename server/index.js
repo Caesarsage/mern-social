ff92d755f839c5fcb8021d9e5be6846e03e8979d
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-04-01 17:57:33 +0100

    structure and initial backend

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose  from 'mongoose'
+import cors from 'cors'
+
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors())
+
+// mongodb
+console.log(process.en)
+const CONNECTION_URL = process.env.MONGO_URL || "mongodb://localhost:27017/mern-social"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+  app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`))
+})
+.catch((e)=>{
+  console.log(e.message);
+})
