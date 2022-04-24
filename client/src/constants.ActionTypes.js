0d02e1aa2be75e46ff26191e3c6568e3cc0dabe3
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-04-24 02:42:56 +0100

    variable constants

---
+++ client/src/constants.ActionTypes.js
@@ -0,0 +1,13 @@
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const FETCH_BY_SEARCH = 'FETCH_BY_SEARCH';
+export const LIKE = 'LIKE';
+export const FETCH_ONE = 'FETCH_ONE'
+
+
+export const AUTH = 'AUTH'
+export const LOGOUT = 'LOGOUT'
+export const START_LOADING = 'START_LOADING'
+export const END_LOADING = 'END_LOADING'
