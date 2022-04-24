efb700d4672b9aa46930b877239b2529b998946a
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-04-24 01:17:16 +0100

    api file for axios conneections

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000/posts'
+const API = axios.create({
+  baseURL: 'http://localhost:5000'
+})
 
-export const fetchPosts = () => axios.get(url)
-export const createPost = (newPost) => axios.post(url, newPost)
-export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, updatePost)
-export const deletePost = (id) => axios.delete(`${url}/${id}`)
-export const likePost = (id) => axios.patch(`${url}/${id}/likes`)
+API.interceptors.request.use((req)=>{
+  if(localStorage.getItem('profile')){
+    const user = JSON.parse(localStorage.getItem("profile"));
+    req.headers.authorization =  `Bearer ${user.token}`
+  }
+
+  return req
+});
+
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`)
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
+export const createPost = (newPost) => API.post('/posts', newPost)
+export const updatePost = (id, updatePost) => API.patch(`/posts/${id}`, updatePost)
+export const deletePost = (id) => API.delete(`/posts/${id}`)
+export const likePost = (id) => API.patch(`/posts/${id}/likes`)
+
+export const login = (formData) => API.post('/user/login', formData)
+export const register = (formData) => API.post('/user/register', formData);
