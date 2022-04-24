39b955b0c4ddcc2ba9f97af12a9795169c67dbc3
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-04-24 01:21:49 +0100

    setup index js file

--- client/src/index.js
+++ client/src/index.js
@@ -13,9 +13,9 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-  {/* <React.StrictMode> */}
+  <React.StrictMode>
     <App />
-  {/* </React.StrictMode>,   */}
+  </React.StrictMode>,  
   </Provider>,
   document.getElementById('root')
 );
